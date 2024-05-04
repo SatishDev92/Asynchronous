469456c2d3ceaca0fa0c57bafbc238e6c0a8d57a
SatishDev92 <kcsatish404@gmail.com>
2024-05-04 17:03:35 +0545

    using async fun

--- joke.js
+++ joke.js
@@ -2,13 +2,8 @@ let jokeContainer= document.getElementById("joke")
 const btn=document.getElementById("btn");
 const url="https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Dark,Pun,Spooky,Christmas?blacklistFlags=nsfw,religious,political,racist,explicit&type=single"
 
-let getJoke= ()=>{
-    fetch(url)
-        .then(data =>data.json())
-        .then(item =>{
-            jokeContainer.innerHTML=`${item.joke}`
-        });
-        
-    
-}
-btn.addEventListener("click",getJoke)
+btn.addEventListener("click", async function (){
+    let raw= await fetch(url);
+    let data= await raw.json();
+    jokeContainer.innerHTML=`${data.joke}`
+   })
